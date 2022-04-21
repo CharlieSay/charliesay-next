7861d0b7ddb4c83a5cf75d8e93b8956957ef41d0
charsay <charlie.say@itv.com>
2022-04-21 19:07:02 +0100

    :tada: Putting foundations in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../styles/theme";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- styles/common.styles.ts
+++ styles/common.styles.ts
@@ -1,8 +1,10 @@
-.container {
-  padding: 0 2rem;
-}
+import styled from "styled-components";
 
-.main {
+export const Container = styled.div`
+  padding: 0 2rem;
+`;
+
+export const Main = styled.main`
   min-height: 100vh;
   padding: 4rem 0;
   flex: 1;
@@ -10,107 +12,87 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
-}
+`;
 
-.footer {
+export const Footer = styled.footer`
   display: flex;
   flex: 1;
   padding: 2rem 0;
   border-top: 1px solid #eaeaea;
   justify-content: center;
   align-items: center;
-}
+`;
 
-.footer a {
+export const FooterLinkA = styled.a`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-grow: 1;
-}
+`;
 
-.title a {
-  color: #0070f3;
+export const TitleH1 = styled.h1`
   text-decoration: none;
-}
-
-.title a:hover,
-.title a:focus,
-.title a:active {
-  text-decoration: underline;
-}
-
-.title {
   margin: 0;
   line-height: 1.15;
   font-size: 4rem;
-}
-
-.title,
-.description {
   text-align: center;
-}
 
-.description {
-  margin: 4rem 0;
-  line-height: 1.5;
+  a {
+    color: #0070f3;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const Description = styled.p`
+  text-align: center;
+  margin: 4rem 0 1rem 0;
   font-size: 1.5rem;
-}
+`;
 
-.code {
+export const Code = styled.code`
   background: #fafafa;
   border-radius: 5px;
-  padding: 0.75rem;
-  font-size: 1.1rem;
+  font-size: 0.8rem;
+  margin: 0rem 0 1rem 0;
   font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
     Bitstream Vera Sans Mono, Courier New, monospace;
-}
+`;
 
-.grid {
+export const Grid = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
   max-width: 800px;
-}
+  @media (max-width: 600px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`;
 
-.card {
+export const Card = styled.a`
   margin: 1rem;
   padding: 1.5rem;
   text-align: left;
   color: inherit;
   text-decoration: none;
   border: 1px solid #eaeaea;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  min-height: 185px;
   border-radius: 10px;
-  transition: color 0.15s ease, border-color 0.15s ease;
   max-width: 300px;
-}
+`;
 
-.card:hover,
-.card:focus,
-.card:active {
-  color: #0070f3;
-  border-color: #0070f3;
-}
-
-.card h2 {
+export const CardH2 = styled.h2`
   margin: 0 0 1rem 0;
   font-size: 1.5rem;
-}
+`;
 
-.card p {
+export const CardP = styled.p`
   margin: 0;
   font-size: 1.25rem;
   line-height: 1.5;
-}
-
-.logo {
-  height: 1em;
-  margin-left: 0.5rem;
-}
-
-@media (max-width: 600px) {
-  .grid {
-    width: 100%;
-    flex-direction: column;
-  }
-}
+`;
